f97d2205db273c136fa50f372eb4ab133a4737fb
Christian Danzmann <cdanzmann@gmail.com>
2013-12-18 20:56:56 +0100

    little fix for url lookup. all urls should end with /.

--- BuLiTipp/BuLiTippApp/urls.py
+++ BuLiTipp/BuLiTippApp/urls.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 
-from .views import NewsPageView
+from views import NewsPageView
 
 urlpatterns = patterns('',
-    url(r'^news$', NewsPageView.as_view(), name='news'),
+    url(r'^news/$', NewsPageView.as_view(), name='news'),
 )
