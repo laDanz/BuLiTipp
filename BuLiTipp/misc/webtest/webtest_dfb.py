291407c659917a00c225a331a50593771ab7ac80
Christian Danzmann <cdanzmann@gmail.com>
2014-02-13 17:24:29 +0100

    update speedtest

---
+++ BuLiTipp/misc/webtest/webtest_dfb.py
@@ -0,0 +1,11 @@
+from django.test import Client
+c = Client()
+response = c.post('/BuLiTipp/login', {'username': 'webtest', 'password': 'webtest'})
+response = c.get('/BuLiTipp/spieltag/1/37/')
+
+if response.status_code == "302":
+	raise("not registered user!")
+
+f=open("response.html","w")
+f.write(response.content)
+f.close()
--- BuLiTipp/speed_test.sh
+++ BuLiTipp/speed_test.sh
@@ -4,5 +4,9 @@ echo "News, reference:"
 time cat misc/webtest/webtest_news.py | ./manage.py shell 2> /dev/null
 echo "Spieltag, full details:"
 time cat misc/webtest/webtest.py | ./manage.py shell 2> /dev/null
+echo "Spieltag(DFB), full details:"
+time cat misc/webtest/webtest_dfb.py | ./manage.py shell 2> /dev/null
 echo "Home, medium details"
 time cat misc/webtest/webtest_home.py | ./manage.py shell 2> /dev/null
+echo "Home(DFB), medium details"
+time cat misc/webtest/webtest_home_dfb.py | ./manage.py shell 2> /dev/null
